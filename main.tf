5569edc40cc7264aea87441a300b700808095e7a
Lesedi <molemilesedi@gmail.com>
2025-12-20 22:03:10 +0200

    feature: create lambda function role and execution policy

--- main.tf
+++ main.tf
@@ -26,3 +26,23 @@ resource "aws_ecr_lifecycle_policy" "market_financials_lifecycle_policy" {
     }]
   })
 }
+
+resource "aws_iam_role" "market_financials_role" {
+  name = "market_financials_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "market_financials_iam_policy" {
+  role = aws_iam_role.market_financials_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
