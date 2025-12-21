3dfbc57a0c509502f17d98d4f6d5fb131722fb50
Lesedi <molemilesedi@gmail.com>
2025-12-21 13:16:01 +0200

    chore: return api endpoint to the gateway

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "repository_url" {
     value = aws_ecr_repository.market_financials.repository_url
     description = "URL to ECR repository"
 }
+
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.market_financials_gw.api_endpoint
+  description = "Endpoint to API"
+}
