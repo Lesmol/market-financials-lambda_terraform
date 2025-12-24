c7001a4bdb2d560feabc4a26c7b5ed4de768df22
Lesedi <molemilesedi@gmail.com>
2025-12-24 17:11:58 +0200

    minor fix to update .name to function in the log group name

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_log_group" "market_financials_cloudwatch" {
 }
 
 resource "aws_cloudwatch_log_group" "market_financials_auth_cloudwatch" {
-  name              = "/aws/lambda/${aws_lambda_function.market_financials_auth_function.name}"
+  name              = "/aws/lambda/${aws_lambda_function.market_financials_auth_function.function_name}"
   retention_in_days = 7
 
   tags = {
