fcaa07ce0f86a7c217fd332b1624234b12851cdc
Lesedi <molemilesedi@gmail.com>
2025-12-24 14:54:33 +0200

    comment out outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "repository_url" {
-    value = aws_ecr_repository.market_financials.repository_url
-    description = "URL to ECR repository"
-}
+# output "repository_url" {
+#     value = aws_ecr_repository.market_financials.repository_url
+#     description = "URL to ECR repository"
+# }
 
-output "api_endpoint" {
-  value = aws_apigatewayv2_api.market_financials_gw.api_endpoint
-  description = "Endpoint to API"
-}
+# output "api_endpoint" {
+#   value = aws_apigatewayv2_api.market_financials_gw.api_endpoint
+#   description = "Endpoint to API"
+# }
 
-output "base_url" {
-  value = aws_apigatewayv2_stage.market_financials_gw_stage.invoke_url
-  description = "Base URL for API Gateway stage"
-}
+# output "base_url" {
+#   value = aws_apigatewayv2_stage.market_financials_gw_stage.invoke_url
+#   description = "Base URL for API Gateway stage"
+# }
